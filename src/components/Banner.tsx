c2360ae1043e9b6826b458758892ffd51230a389
Saydia <sadiatasnim646@gmail.com>
2026-09-07 13:02:47 +0600

    add banner

---
+++ src/components/Banner.tsx
@@ -0,0 +1,16 @@
+import BannerImage from '../assets/Hero Section 1.png';
+
+const Banner = () => {
+    return (
+  <div className='text-center'>
+         <div className=' mx-auto py-6'>
+            <h1 className="text-6xl font-bold ">Freshness <span className="text-green-500">You Can <br />
+                Count</span> On, Prices You’ll Love!</h1>
+            <p className='max-w-[40%] mx-auto py-6'>Shop your daily essentials at unbeatable prices. From fresh produce to pantry staples, we’ve got you covered every day!</p>
+            
+        </div>
+        <img src={BannerImage} className='mx-auto max-w-[450] md:max-w-[920px] h-auto' alt="Banner" />
+  </div>
+    )
+}
+export default Banner;
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from './components/Navbar'
+import Banner from './components/Banner'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
    <Navbar></Navbar>
+   <Banner></Banner>
    <h2>Banner section</h2>
   </StrictMode>,
 )
